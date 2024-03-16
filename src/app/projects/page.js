89b811c3350f44e12d9d58eca70488d9bb3c2ae9
SatyamGade <131511270+SatyamGade@users.noreply.github.com>
2024-03-16 11:20:59 +0530

    iNotebook project added

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -12,16 +12,16 @@ function Projects() {
       <h1 className={styles.mainHeading}>Projects</h1>
       <div className={styles.projectDiv}>
         <div className={styles.project}>
-          <div className={styles.imgDiv}><Image src={task} alt='taskTracker' className={styles.img}/></div>
-          <h3 className={styles.heading}>Task Tracker</h3>
-          <p className={styles.description}>In Task Tracker, you can add, edit, delete and filter your tasks. This project created using ReactJS and Bootstrap on local storage.</p>
+          <div className={styles.imgDiv}><Image src={task} alt='iNotebook' className={styles.img} /></div>
+          <h3 className={styles.heading}>iNotebook</h3>
+          <p className={styles.description}>In iNotebook, you can add, edit, delete and update your notes. This is MERN stack project.</p>
           <div className={styles.linkDiv}>
-            <Link href='https://github.com/SatyamGade/TaskTracker' target='_blank' className={styles.link}>Source Code</Link>
-            <Link href='https://satyamgade.github.io/TaskTracker/' target='_blank' className={styles.link}>Visit Website</Link>
+            <Link href='https://github.com/SatyamGade/iNotebook-frontend' target='_blank' className={styles.link}>Source Code</Link>
+            <Link href='https://i-notebook-frontend-six.vercel.app/' target='_blank' className={styles.link}>Visit Website</Link>
           </div>
         </div>
         <div className={styles.project}>
-          <div className={styles.imgDiv}><Image src={news} alt='news24/7' className={styles.img}/></div>
+          <div className={styles.imgDiv}><Image src={news} alt='news24/7' className={styles.img} /></div>
           <h3 className={styles.heading}>News 24/7</h3>
           <p className={styles.description}>In News 24/7, you can read latest news according to the categories. This project created using ReactJS and Bootstrap.</p>
           <div className={styles.linkDiv}>
@@ -30,21 +30,21 @@ function Projects() {
           </div>
         </div>
         <div className={styles.project}>
-          <div className={styles.imgDiv}><Image src={textutils} alt='textUtils' className={styles.img}/></div>
-          <h3 className={styles.heading}>Text Utils</h3>
-          <p className={styles.description}>In this project, you can manupulate your text. This project created using React JS and Bootstrap.</p>
-          <div className={styles.linkDiv}>
-            <Link href='https://github.com/SatyamGade/TextUtils' target='_blank' className={styles.link}>Source Code</Link>
-            <Link href='https://satyamgade.github.io/TextUtils/' target='_blank' className={styles.link}>Visit Website</Link>
-          </div>
-        </div>
-        <div className={styles.project}>
-          <div className={styles.imgDiv}><Image src={clock} alt='clock&weather' className={styles.img}/></div>
+          <div className={styles.imgDiv}><Image src={clock} alt='clock&weather' className={styles.img} /></div>
           <h3 className={styles.heading}>World Clock & Weather</h3>
           <p className={styles.description}>In this project, you can check time and wheather of countries. This project created using HTML, CSS and JavaScript.</p>
           <div className={styles.linkDiv}>
             <Link href='https://github.com/SatyamGade/Clock-Weather' target='_blank' className={styles.link}>Source Code</Link>
             <Link href='https://satyamgade.github.io/Clock-Weather/' target='_blank' className={styles.link}>Visit Website</Link>
+          </div>
+        </div>
+        <div className={styles.project}>
+          <div className={styles.imgDiv}><Image src={textutils} alt='textUtils' className={styles.img} /></div>
+          <h3 className={styles.heading}>Text Utils</h3>
+          <p className={styles.description}>In this project, you can manupulate your text. This project created using React JS and Bootstrap.</p>
+          <div className={styles.linkDiv}>
+            <Link href='https://github.com/SatyamGade/TextUtils' target='_blank' className={styles.link}>Source Code</Link>
+            <Link href='https://satyamgade.github.io/TextUtils/' target='_blank' className={styles.link}>Visit Website</Link>
           </div>
         </div>
       </div>
